320622fc2c2b0773751a68fc95909d96370643e3
quantimnot <quantimnot@users.noreply.github.com>
2021-10-11 00:04:40 -0400

    refactor

---
+++ bin/git-unshallow.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git_unshallow() {
+fd -HI '^\.git$' |
+while read repo
+do
+	repo=$(dirname "${repo}")
+	(
+		cd "${repo}"
+		[ -z "$(git status --porcelain=v1 2>/dev/null)" ] || echo "${repo}"
+	)
+done
+}
+export MAIN=
